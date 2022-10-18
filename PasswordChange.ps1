ed5175178c9bf403a3b0aaf1a85ff3658b6b8825
AresX0 <jdsay@jdsay.com>
2022-10-18 12:07:55 -0500

    Update PasswordChange.ps1
    Fixed issue where script would error out if group membership exceeded 5k users.

--- PasswordChange.ps1
+++ PasswordChange.ps1
@@ -7,13 +7,15 @@ write-verbose "Change Password for Users in the group specified"
   
 $group = Read-Host -Prompt 'Input the Group name'
 
-$users =Get-ADGroupMember -identity "$group" | select DistinguishedName
+$Users = Get-ADGroup "$group" -Properties Member | 
+Select-Object -ExpandProperty Member |
+Get-ADUser | select DistinguishedName
 
 
 foreach ($user in $users) {
     $outObject = new-object -typename psobject
     $outobject | Add-member -MemberType NoteProperty -Name distinguishedName -Value $user.distinguishedname
     $outobject | Add-Member -MemberType NoteProperty -Name OldPasswordLastSet -value $user.passwordlastset
-    set-aduser $user -ChangePasswordAtLogon:$true
+    set-aduser $user -ChangePasswordAtLogon:$true 
     $outObject
 } 
